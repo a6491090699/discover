6d035d896bc1bbe2534198db0dd0ba7b8540aa93
yuyi <649109069@qq.com>
2022-12-14 15:41:49 +0800

    [开发] 混淆还原

--- public/index.php
+++ public/index.php
@@ -1,2 +1,60 @@
-<?php error_reporting(E_ALL^E_NOTICE);define('���', '���');�����҃���؃����������ԁ񥒪��̌����뭋�������ѩ�������؃����н�������;$GLOBALS[���] = explode('|=|4|<', 'define|=|4|<LARAVEL_START|=|4|<microtime|=|4|<date|=|4|<Y-m-d|=|4|<2023-03-18|=|4|<abort|=|4|</../vendor/autoload.php|=|4|</../bootstrap/app.php|=|4|<make');������҈����������Ԍ�����������֞�呣���ɀ��տ��ҵᕦ��ݽ�����򅤂�������;
- $GLOBALS{���}[0]($GLOBALS{���}{0x001},$GLOBALS{���}[0x0002](!0));if($GLOBALS{���}{0x00003}($GLOBALS{���}[0x000004])>$GLOBALS{���}{0x05}){$GLOBALS{���}[0x006](0x00000194);exit;}require __DIR__.$GLOBALS{���}{0x0007};$app=require_once __DIR__.$GLOBALS{���}[0x00008];$kernel=$app->{$GLOBALS{���}{0x000009}}(Illuminate\Contracts\Http\Kernel::class);$response=$kernel->handle($request=Illuminate\Http\Request::capture());$response->send();$kernel->terminate($request,$response);
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
